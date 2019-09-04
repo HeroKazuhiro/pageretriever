4e5ed033cac637f86d6b85f7b33b2fae1b315ecd
yauheni_semiarenka <yauheni_semiarenka@epam.com>
2019-09-04 13:34:29 +0300

    init commit

---
+++ src/main/java/com/example/pageretriever/PageretrieverApplication.java
@@ -0,0 +1,13 @@
+package com.example.pageretriever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PageretrieverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PageretrieverApplication.class, args);
+    }
+
+}
